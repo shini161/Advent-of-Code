f5fae4c66f65747dcd916128b105b7564bcc6c4b
shini161 <85759040+shini161@users.noreply.github.com>
2023-12-06 16:38:52 +0100

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,26 +1,43 @@
+use std::collections::VecDeque;
+
 fn main() {
     let file = std::fs::read_to_string("./input.txt").unwrap();
-
     println!("{}", advent(&file));
 }
 
 fn advent(input: &str) -> u32 {
-    let mut sum = 0;
+    let numbers = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let mut sol: u32 = 0;
 
     for line in input.lines() {
-        for c in line.chars() {
-            if let Some(digit) = c.to_digit(10) {
-                sum += digit * 10;
-                break;
+        let mut deque = VecDeque::new();
+        let mut i = 0;
+
+        while i < line.len() {
+            if let Some(c) = line[i..].chars().next() {
+                if c.is_digit(10) {
+                    deque.push_back(c.to_digit(10).unwrap() as i64);
+                    i += 1;
+                } else {
+                    for (j, n) in numbers.iter().enumerate() {
+                        if line[i..].starts_with(n) {
+                            deque.push_back(j as i64);
+                            i += 1;
+                            break;
+                        }
+                    }
+                    i += 1;
+                }
             }
         }
-        for c in line.chars().rev() {
-            if let Some(digit) = c.to_digit(10) {
-                sum += digit;
-                break;
-            }
+
+        if let (Some(first), Some(last)) = (deque.front(), deque.back()) {
+            sol += (first * 10 + last) as u32;
         }
     }
 
-    sum
+    sol
 }
